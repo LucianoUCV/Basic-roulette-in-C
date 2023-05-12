06ace6ba9ca76c70c22396f5195b783041d533bb
Luciano <luciangligorascu@gmail.com>
2023-05-12 17:35:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Basic Roulette in C
+ This is my first repository. A simple text European roulette ( basically a number guesser from 0 to 36 ). Maybe I'll implement new features in the future ( maybe ).
---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int number, input,sum,stake;
+    printf("$ Enter the sum you want to gamble: ");
+    scanf("%d",&sum);
+    printf("$ Enter the stake: ");
+    scanf("%d",&stake);
+    if(stake>sum)
+    {
+        printf("You don't have enough money!");
+        return 0;
+    }
+    srand(time(NULL));
+    printf("Good luck!");
+    number=rand()%36;
+    do
+    {
+        printf("\nEnter the number: ");
+        scanf("%d",&input);
+        if(number!=input)
+        {
+            printf("You lost %d dollars",stake);
+            sum-=stake;
+        }
+        if(sum<stake&&sum)
+        {
+            printf("\nYou don't have enough money. The stake will be changed!");
+            sum=stake;
+        }
+    }while((number!=input)&&sum);
+    if(sum==0)
+    {
+        printf("\nYou lost everything! :(");
+        return 0;
+    }
+    printf("\n$$$ Congratulations! $$$ You won %d dollars!",36*stake);
+    return 0;
+}
